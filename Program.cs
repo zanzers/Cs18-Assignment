658fc0339d3036e5484d67eb0c73ba1871d1a4b7
zanzers <nowellsaavedra999@gmail.com>
2025-09-26 08:21:24 +0800

    Part1-Done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+namespace cs18_Assignment
+{
+    class Program
+    {
+
+        public class Map
+        {
+            private enum CellType { Empty, Dirt, Obstacle, Cleaned }
+            private CellType[,] _grid;
+            public int Width { get; private set; }
+            public int Height { get; private set; }
+            
+        
+        public Map(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+
+            _grid = new CellType[width, height];
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+
+                _grid[x, y] = CellType.Empty;    
+               }
+            }
+        }
+
+
+
+
+
+        }
+
+
+        public static void Main(string[] args)
+        {
+            
+            Map myMap = new Map(12, 15);
+
+            Console.WriteLine($"Grid Width is {myMap.Width}");
+            Console.WriteLine($"Grid Height is {myMap.Height}");
+        }
+    }
+
+} 
